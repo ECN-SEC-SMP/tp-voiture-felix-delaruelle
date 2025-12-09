3bbfe9f04652794cf1975ac26e71458d66258826
hugodlrll <hugo.del49@gmail.com>
2025-12-09 09:40:01 +0100

    Q6

--- autoroute.py
+++ autoroute.py
@@ -1,11 +1,18 @@
 # openCV import
 import cv2 as cv
 import sys
-
+import numpy as np
 
 # Keycode definitions
 ESC_KEY = 27
 Q_KEY = 113
+
+# Print video information
+def videoInfo(cap):
+    print("N (number of frames) : " + str(cap.get(cv.CAP_PROP_FRAME_COUNT)))
+    print("length : " + str(cap.get(cv.CAP_PROP_FRAME_COUNT) / cap.get(cv.CAP_PROP_FPS)))
+    print("width : " + str(cap.get(cv.CAP_PROP_FRAME_WIDTH)))
+    print("height : " + str(cap.get(cv.CAP_PROP_FRAME_HEIGHT)))
 
 def main():
     # Define variables
@@ -13,7 +20,8 @@ def main():
 
     # Reading the image (and forcing it to grayscale)
     cap = cv.VideoCapture(filename)
-
+    videobuf = np.empty((int(cap.get(cv.CAP_PROP_FRAME_COUNT)), int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv.CAP_PROP_FRAME_WIDTH)), 3), dtype=np.uint8)
+    
     run = cap.isOpened()
    # Making sure the capture has opened successfully
     if not run:
@@ -21,30 +29,27 @@ def main():
         print("capture opening has failed we cannot do anything :'(")
         sys.exit()
 
-    print("N : " + str(cap.get(cv.CAP_PROP_FRAME_COUNT)))
-    print("length : " + str(cap.get(cv.CAP_PROP_FRAME_COUNT) / cap.get(cv.CAP_PROP_FPS)))
-    print("width : " + str(cap.get(cv.CAP_PROP_FRAME_WIDTH)))
-    print("height : " + str(cap.get(cv.CAP_PROP_FRAME_HEIGHT)))
+    videoInfo(cap)
     #Creating a window to display some images
     cv.namedWindow("Original video")
     cv.namedWindow("Gray video")
     
     # A key that we use to store the user keyboard input
     key = None
+    fc = 0
+    ret = True  
     # Waiting for the user to press ESCAPE before exiting the application
     
-    while key != ESC_KEY and key!= Q_KEY:
-        ret, im = cap.read()
-        # Turning im into grayscale and storing it in imGray
-        # TODO
-        
-        
-        cv.imshow("Original video", im)
-        #imshow("Gray video", imGray)
-        
+    while key != ESC_KEY and key!= Q_KEY and fc < int(cap.get(cv.CAP_PROP_FRAME_COUNT)) and ret:
+        # store the video in a numpy array
+        ret, videobuf[fc] = cap.read()
+        imGray = cv.cvtColor(videobuf[fc], cv.COLOR_RGB2GRAY)
+        cv.imshow("Original video", videobuf[fc])
+        cv.imshow("Gray video", imGray)
         # Look for pollKey documentation
         key = cv.pollKey()
-    
+        # Increment frame counter
+        fc += 1
     
     # release cap
     cap.release()
